9dc362e28ee4d26001e78291a1864bae794a4ecc
LinZH-1995 <lin84610@gmail.com>
2023-03-14 14:37:28 +0800

    feat: add layout create button and index UI

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const mongoose = require('mongoose')
+const Restaurant = require('./models/restaurant.js')
 const app = express()
 const port = 3000
 
@@ -18,9 +19,13 @@ db.once('open', () => {
 app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 app.use(express.urlencoded({ extended: true }))
+app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-  res.render('index')
+  return Restaurant.find()
+    .lean()
+    .then(restaurants => res.render('index', { restaurants }))
+    .catch(error => console.error(error))
 })
 
 app.listen(port, () => {
