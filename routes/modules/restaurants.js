a1840ed912413db9cc24c8085f6e3ceded21e614
LinZH-1995 <lin84610@gmail.com>
2023-03-16 17:14:27 +0800

    refactor: change condition id to _id

--- routes/modules/restaurants.js
+++ routes/modules/restaurants.js
@@ -3,23 +3,12 @@ const router = express.Router()
 const Restaurant = require('../../models/restaurant.js')
 
 router.get('/new', (req, res) => {
-  return Restaurant.find()
-    .lean()
-    .then(restaurants => {
-      const targetID = restaurants.reduce((acc, cur) => {
-        const id = Math.max(acc, Number(cur.id))
-        return id
-      }, 0)
-      return targetID
-    })
-    .then(id => res.render('new', { ID: id + 1 }))
-    .catch(error => console.error(error))
+  return res.render('new')
 })
 
 router.post('/', (req, res) => {
   const newData = req.body
   return Restaurant.create({
-    id: newData.id,
     name: newData.name,
     category: newData.category,
     image: newData.image,
@@ -35,7 +24,7 @@ router.post('/', (req, res) => {
 
 router.get('/:id/detail', (req, res) => {
   const id = req.params.id
-  return Restaurant.findOne({ id: id })
+  return Restaurant.findById(id)
     .lean()
     .then(restaurant => res.render('detail', { restaurant }))
     .catch(error => console.error(error))
@@ -43,7 +32,7 @@ router.get('/:id/detail', (req, res) => {
 
 router.get('/:id/edit', (req, res) => {
   const id = req.params.id
-  return Restaurant.findOne({ id: id })
+  return Restaurant.findById(id)
     .lean()
     .then(restaurant => res.render('edit', { restaurant }))
     .catch(error => console.error(error))
@@ -52,18 +41,18 @@ router.get('/:id/edit', (req, res) => {
 
 router.put('/:id', (req, res) => {
   const id = req.params.id
-  const newData = new Restaurant(req.body)
-  return Restaurant.findOne({ id: id })
+  const newData = req.body
+  return Restaurant.findById(id)
     .then(restaurant => {
-      restaurant.id = id
-      restaurant.name = newData.name
-      restaurant.category = newData.category
-      restaurant.image = newData.image
-      restaurant.location = newData.location
-      restaurant.phone = newData.phone
-      restaurant.google_map = newData.google_map
-      restaurant.rating = newData.rating
-      restaurant.description = newData.description
+      // restaurant.name = newData.name
+      // restaurant.category = newData.category
+      // restaurant.image = newData.image
+      // restaurant.location = newData.location
+      // restaurant.phone = newData.phone
+      // restaurant.google_map = newData.google_map
+      // restaurant.rating = newData.rating
+      // restaurant.description = newData.description
+      restaurant = Object.assign(restaurant, newData)
       return restaurant.save()
     })
     .then(() => res.redirect(`/restaurants/${id}/detail`))
@@ -72,25 +61,7 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   const id = req.params.id
-  return Restaurant.findOne({ id })
-    .then(restaurant => {
-      restaurant.deleteOne()
-    })
-    .then(() => {
-      Restaurant.find()
-        .then(restaurants => {
-          restaurants.forEach(restaurant => {
-            if (restaurant.id > id) {
-              restaurant.id = (Number(restaurant.id) - 1).toString()
-            }
-          })
-          restaurants.forEach(restaurant => {
-            const newData = new Restaurant(restaurant)
-            newData.save()
-          })
-        })
-
-    })
+  return Restaurant.deleteOne({ _id: id })
     .then(() => res.redirect('/'))
     .catch(error => console.error(error))
 })
