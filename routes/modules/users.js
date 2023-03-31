978cb9c47ca279ffc337ef134564888f3f70219d
LinZH-1995 <lin84610@gmail.com>
2023-03-31 19:09:16 +0800

    feat: add login and register feature

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,12 +1,39 @@
 const express = require('express')
 const router = express.Router()
+const User = require('../../models/user.js')
 
 router.get('/login', (req, res) => {
   res.render('login')
 })
 
+router.post('/login', (req, res) => {
+  const { email, password } = req.body
+  return User.findOne({ email })
+    .then(user => {
+      if (!user) return res.redirect('/users/login')
+      if (user.password !== password ) return res.render('login', { email })
+
+      return res.redirect('/')
+    })
+    .catch(err => console.log(err))
+})
+
 router.get('/register', (req, res) => {
   res.render('register')
+})
+
+router.post('/register', (req, res) => {
+  const { name, email, password, confirmPassword } = req.body
+
+  return User.findOne({ email })
+    .then(user => {
+      if (user) return res.render('register', { name, email, password, confirmPassword })
+
+      return User.create({ name, email, password })
+        .then(() => res.redirect('/users/login'))
+        .catch(err => console.log(err))
+    })
+    .catch(err => console.log(err))
 })
 
 module.exports = router
