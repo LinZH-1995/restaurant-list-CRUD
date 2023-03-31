72cfeff57c12412151f07b62e30d8bbe07eff8f7
LinZH-1995 <lin84610@gmail.com>
2023-03-31 23:25:57 +0800

    feat: add relation between User and Restaurant model

--- routes/modules/homePage.js
+++ routes/modules/homePage.js
@@ -5,18 +5,20 @@ const Restaurant = require('../../models/restaurant.js')
 router.use(express.urlencoded({ extended: true }))
 
 router.get('/', (req, res) => {
-  return Restaurant.find()
+  const userId = req.user._id
+  return Restaurant.find({ userId })
     .lean()
     .then(restaurants => res.render('index', { restaurants }))
     .catch(error => console.error(error))
 })
 
 router.get('/search', (req, res) => {
+  const userId = req.user._id
   const keyword = req.query.keyword
   // restaurantList.results.name
   // restaurantList.results.name_en
   // restaurantList.results.category
-  return Restaurant.find()
+  return Restaurant.find({ userId })
     .lean()
     .then(restaurants => {
       return restaurants.filter(restaurant => 
--- routes/modules/restaurants.js
+++ routes/modules/restaurants.js
@@ -7,32 +7,26 @@ router.get('/new', (req, res) => {
 })
 
 router.post('/', (req, res) => {
+  const userId = req.user._id
   const newData = req.body
-  return Restaurant.create({
-    name: newData.name,
-    category: newData.category,
-    image: newData.image,
-    location: newData.location,
-    phone: newData.phone,
-    google_map: newData.google_map,
-    rating: Number(newData.rating),
-    description: newData.description
-  })
+  return Restaurant.create(Object.assign({ userId }, newData))
     .then(() => res.redirect('/'))
     .catch(error => console.error(error))
 })
 
 router.get('/:id/detail', (req, res) => {
-  const id = req.params.id
-  return Restaurant.findById(id)
+  const userId = req.user._id
+  const _id = req.params.id
+  return Restaurant.findOne({ userId, _id })
     .lean()
     .then(restaurant => res.render('detail', { restaurant }))
     .catch(error => console.error(error))
 })
 
 router.get('/:id/edit', (req, res) => {
-  const id = req.params.id
-  return Restaurant.findById(id)
+  const userId = req.user._id
+  const _id = req.params.id
+  return Restaurant.findOne({ userId, _id })
     .lean()
     .then(restaurant => res.render('edit', { restaurant }))
     .catch(error => console.error(error))
@@ -40,58 +34,54 @@ router.get('/:id/edit', (req, res) => {
 })
 
 router.put('/:id', (req, res) => {
-  const id = req.params.id
+  const userId = req.user._id
+  const _id = req.params.id
   const newData = req.body
-  return Restaurant.findById(id)
+  return Restaurant.findOne({ userId, _id })
     .then(restaurant => {
       // restaurant.name = newData.name
-      // restaurant.category = newData.category
-      // restaurant.image = newData.image
-      // restaurant.location = newData.location
-      // restaurant.phone = newData.phone
-      // restaurant.google_map = newData.google_map
-      // restaurant.rating = newData.rating
-      // restaurant.description = newData.description
       restaurant = Object.assign(restaurant, newData)
       return restaurant.save()
     })
-    .then(() => res.redirect(`/restaurants/${id}/detail`))
+    .then(() => res.redirect(`/restaurants/${_id}/detail`))
     .catch(error => console.error(error))
 })
 
 router.delete('/:id', (req, res) => {
-  const id = req.params.id
-  return Restaurant.deleteOne({ _id: id })
+  const userId = req.user._id
+  const _id = req.params.id
+  return Restaurant.deleteOne({ userId, _id })
     .then(() => res.redirect('/'))
     .catch(error => console.error(error))
 })
 
 router.get('/sort/:condition', (req, res) => {
+  const userId = req.user._id
   const condition = req.params.condition
   switch (condition) {
     case 'A-Z':
-      return Restaurant.find()
+      return Restaurant.find({ userId })
         .lean()
         .sort({ name: 'asc' })
         .then(restaurants => res.render('index', { restaurants }))
         .catch(error => console.error(error))
 
     case 'Z-A':
-      return Restaurant.find()
+      return Restaurant.find({ userId })
         .lean()
         .sort({ name: 'desc' })
         .then(restaurants => res.render('index', { restaurants }))
         .catch(error => console.error(error))
 
     case 'category':
-      return Restaurant.find()
+      return Restaurant.find({ userId })
         .lean()
         .sort({ category: 'asc' })
         .then(restaurants => res.render('index', { restaurants }))
         .catch(error => console.error(error))
 
     case 'rating':
-      return Restaurant.find()
+      return Restaurant.find({ userId })
         .lean()
         .sort({ rating: 'desc' })
         .then(restaurants => res.render('index', { restaurants }))
