c9b4b5b1accfd70b55d497db0f3fb8b1de684a69
LinZH-1995 <lin84610@gmail.com>
2023-03-31 18:41:25 +0800

    feat: add login and register route & style: change login and register UI

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,10 @@ const { route } = require('./modules/homePage.js')
 const router = express.Router()
 const homePage = require('./modules/homePage.js')
 const restaurants = require('./modules/restaurants.js')
+const users = require('./modules/users.js')
 
-router.use('/', homePage)
 router.use('/restaurants', restaurants)
+router.use('/users', users)
+router.use('/', homePage)
 
 module.exports = router
