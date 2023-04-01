c43cf332e80b33a6186d001dcb08626b01ab9b63
LinZH-1995 <lin84610@gmail.com>
2023-04-01 22:28:09 +0800

    fix: fix register redirect bug

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -36,7 +36,7 @@ router.post('/register', (req, res) => {
       return bcrypt.genSalt(10)
         .then(salt => bcrypt.hash(password, salt))
         .then(hash => User.create({ name, email, password: hash }))
-        .then(() => res.redirect('/'))
+        .then(() => res.redirect('/users/login'))
         .catch(err => console.log(err))
     })
     .catch(err => console.log(err))
