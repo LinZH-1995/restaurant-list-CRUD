d7025620e3eecd154fdad556b4b8588f561c925f
LinZH-1995 <lin84610@gmail.com>
2023-04-01 13:12:44 +0800

    feat: add Google Login Strategy

--- routes/modules/auth.js
+++ routes/modules/auth.js
@@ -6,7 +6,16 @@ router.get('/facebook', passport.authenticate('facebook', {
   scope: ['email', 'public_profile']
 }))
 
+router.get('/google', passport.authenticate('google', {
+  scope: ['email', 'profile']
+}))
+
 router.get('/facebook/callback', passport.authenticate('facebook', {
+  successRedirect: '/',
+  failureRedirect: '/users/login'
+}))
+
+router.get('/google/callback', passport.authenticate('google', {
   successRedirect: '/',
   failureRedirect: '/users/login'
 }))
